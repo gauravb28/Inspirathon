3374b7af01e63dfcd6afe76ac6a39ea27a1110a8
universej2002@gmail.com <universej2002@gmail.com>
2021-11-12 14:20:30 +0530

    added slider,nav

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from "./Components/Header"
+import Slider from "./Components/SliderC"
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Slider/>
+    </div>
+  );
+}
+
+export default App;
